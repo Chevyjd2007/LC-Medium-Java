df9448bc3fc8ff4c06fadfe72031f0d65b603511
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-10-11 22:06:09 -0400

    +MaximumSubarray

--- src/com/Chevy/MaximumSubarray.java
+++ src/com/Chevy/MaximumSubarray.java
@@ -1,11 +1,28 @@
+package com.Chevy;
+
 public class MaximumSubarray {
     public static void main(String[] args ) {
 
     }
 
     public static int maxSubArrau(int[] nums) {
-        int n = nums.length;
+        // We can't instatiate largestSum to 0 because there may be negative numbers.
+        int largestSum = nums[0];
+        // This is the integer value that will be used to check if the summation of the current subarray.
+        int currentSum = 0;
 
 
+        // We iterate through the nums array resetting currentSum to 0 if it's a negative value.
+        // If not, we add it to largest sum.
+        for (int num : nums) {
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+
+            currentSum += num;
+            largestSum = Math.max(largestSum, currentSum);
+        }
+
+        return largestSum;
     }
 }
